b0c909e2e443a29d6a3d388d92237e5091bea0e6
purushseva <purushseva@gmail.com>
2025-07-08 14:02:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,11 +81,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/onlynaman for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/+RNPh1LbP8QZlYmU1 for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/onlynaman")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ig_mentor")
         self.LOGGER(__name__).info(f"""       
 
 
@@ -99,7 +99,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @ONLYNAMAN")   
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @ig_mentor")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -107,7 +107,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @ONLYNAMAN</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @ig_mentor</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
@@ -118,7 +118,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @ONLYNAMAN")
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @ig_mentor")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
